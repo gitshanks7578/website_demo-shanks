1d646537c4be7ee10160c0e468892d9d2fc50313
gitshanks7578 <shashanku346@gmail.com>
2025-02-19 01:42:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -129,4 +129,19 @@ gsap.from(".animate",{
     }
 })
  
+ 
+const lenis = new Lenis({
+    duration:1.2
+});
 
+// Listen for the scroll event and log the event data
+lenis.on('scroll', (e) => {
+    console.log(e);
+});
+
+function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
